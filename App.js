52c01cc8434f125cedf2b3d346002ff682ad6e2c
ronnie <ronnie.willyams.rws@gmail.com>
2024-05-12 08:02:53 -0300

    Primeiro commit do projeto

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {ScrollView} from 'react-native';
+import {Container,
+        TxtContainer,
+        Input,
+        BtnCadastro,
+        TxtCadastro,
+        TabelaCadastro,
+        LinhaCadastro,
+        TxtResultado} from './src/styles'
 
-export default function App() {
+const CadastroEventos = () => {
+  const [nomeEvento, setNomeEvento] = useState('');
+  const [dataEvento, setDataEvento] = useState('');
+  const [localEvento, setLocalEvento] = useState('');
+  const [eventosCadastrados, setEventosCadastrado] = useState([
+      {
+         nome:' Nome do evento ',
+         data: ' Data do evento ',
+         local:' Local do evento '
+      }
+  ]);
+
+  const cadastrarEvento = () => {
+    
+    setNomeEvento('Nome do evento:', nomeEvento);
+    setDataEvento('Data do evento:', dataEvento);
+    setLocalEvento('Local do evento:', localEvento);
+    setNomeEvento('');
+    setDataEvento('');
+    setLocalEvento('');
+  };
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+     <ScrollView style={{marginTop:50}}>
+      <Container>
+        <TxtContainer>Nome do Evento:</TxtContainer>
+        <Input
+          placeholder="Digite o nome do evento"
+          value={nomeEvento}
+          onChangeText={setNomeEvento}
+        />
+        
+        <TxtContainer>Data do Evento:</TxtContainer>
+        <Input
+          placeholder="Digite a data do evento"
+          value={dataEvento}
+          onChangeText={setDataEvento}
+        />
+        
+        <TxtContainer>Local do Evento:</TxtContainer>
+        <Input
+          placeholder="Digite o local do evento"
+          value={localEvento}
+          onChangeText={setLocalEvento}
+        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <BtnCadastro onPress={cadastrarEvento}>
+          <TxtCadastro>Cadastrar Evento</TxtCadastro>
+        </BtnCadastro>
+
+        <TabelaCadastro>
+          {eventosCadastrados.map((evento, index) => (
+            <LinhaCadastro key={index}>
+              <TxtResultado>{`Nome do evento: ${evento.nome}`}</TxtResultado>
+              <TxtResultado>{`Data do evento: ${evento.data}`}</TxtResultado>
+              <TxtResultado>{`Local do evento: ${evento.local}`}</TxtResultado>
+            </LinhaCadastro>
+          ))}
+        </TabelaCadastro>
+          
+      </Container>
+     </ScrollView>
+  );
+};
+
+export default CadastroEventos;
---
+++ src/styles.js
@@ -0,0 +1,63 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+   flex:1;
+   background-color:#FFF;
+   align-items:center;
+   justify-content:center;
+`;
+
+export const TxtContainer = styled.Text`
+   color:#000;
+   font-size:18px;
+   margin-top:10px;
+`;
+
+export const Input = styled.TextInput`
+    width:90%;
+    height:40px;
+    border:1px solid #ccc;
+    margin-top:10px;
+    padding-left:8px;
+`;
+
+export const BtnCadastro = styled.TouchableOpacity`
+     width:90%;
+     height:50px;
+     background-color:#3b3dbf;
+     margin-top:15px;
+     border-radius:6px;
+`;
+
+export const TxtCadastro = styled.Text`
+    color: #fff;
+    font-size:20px;
+    text-align:center;
+    padding:10px;
+`;
+
+export const TabelaCadastro = styled.View`
+    width:85%;
+    height:200px;
+    background-color:gray;
+    padding:10px;
+    margin-top:20px;
+`;
+
+export const LinhaCadastro = styled.Text`
+   color:#fff;
+   padding:5px 6px;
+   font-size:18px;
+   
+`;
+
+export const TxtResultado = styled.Text`
+    margin:20px 5px;
+`;
+
+
+
+
+
+
+
